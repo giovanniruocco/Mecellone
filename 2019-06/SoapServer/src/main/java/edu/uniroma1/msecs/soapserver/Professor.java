25cc603e137356d4e4067df1ffbfc0ceb6f8d562
Alessandro Serpi <alessandro.serpi@me.com>
2019-07-20 17:21:49 +0200

    Add Professor class

---
+++ 2019-06/SoapServer/src/main/java/edu/uniroma1/msecs/soapserver/Professor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uniroma1.msecs.soapserver;
+
+/**
+ *
+ * @author biar
+ */
+public class Professor {
+    private String name;
+    private String surname;
+
+    public Professor(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
