cfbd657cbe76a6eb225036ef30f2fde3ecbb57db
giovanniruocco <45178170+giovanniruocco@users.noreply.github.com>
2020-07-16 10:38:19 +0200

    Update DatabaseManager.java

--- 2019-09/DatabaseManager/src/main/java/it/uniroma1/msecs/ay2019/e3/DatabaseManager.java
+++ 2019-09/DatabaseManager/src/main/java/it/uniroma1/msecs/ay2019/e3/DatabaseManager.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.uniroma1.msecs.ay2019.e3;
 
 import java.sql.*;
 
-/**
- *
- * @author biar
- */
 public class DatabaseManager {
     public static void main(String[] args) throws ClassNotFoundException {
         if (args.length < 1) {
