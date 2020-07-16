80a70eb6368cac8ee5ed10034f4a39dc1937bf4e
giovanniruocco <45178170+giovanniruocco@users.noreply.github.com>
2020-07-16 10:44:41 +0200

    Update Professor.java

--- 2019-06/SoapServer/src/main/java/edu/uniroma1/msecs/soapserver/Professor.java
+++ 2019-06/SoapServer/src/main/java/edu/uniroma1/msecs/soapserver/Professor.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.uniroma1.msecs.soapserver;
-
 import java.util.HashMap;
 import java.util.Map;
 import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- *
- * @author biar
- */
 @XmlRootElement
 public class Professor {
     private String name;
@@ -27,23 +16,18 @@ public class Professor {
         this.name = name;
         this.surname = surname;
     }
-
     public String getName() {
         return name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-    
     public static Professor getProfessor(String id) {
         return professors.get(id);
     }
